30a0695929c6e234c898295dc2d47abd9f58fc19
Stan Angeloff <stanimir@angeloff.name>
2025-02-19 23:21:54 +0200

    fix: fix message selection keybinding

--- lua/claudius/init.lua
+++ lua/claudius/init.lua
@@ -222,7 +222,7 @@ local default_config = {
       next_message = "]m", -- Jump to next message
       prev_message = "[m", -- Jump to previous message
       select_in_message = "vim", -- Select inside message content
-      select_message = "vaM", -- Select entire message including prefix
+      select_message = "vam", -- Select entire message including prefix
     },
     insert = {
       send = "<C-]>",
